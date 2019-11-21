f7722160a64d8f27f0e57f6d337afd5de1a2a2ac
James Garrison <jamesgarrison@guest-wl-dhcp-155-239.mines.edu>
2019-11-21 09:45:18 -0700

    LoadFile Function

--- week12.py
+++ week12.py
@@ -2,5 +2,23 @@
 # CSCI 102 - Section C
 # Week 12 - utility
 
+
+
+# Function 1
 def PrintOutput(a):
     print('OUTPUT', a)
+
+
+# Function 2
+def LoadFile(filename):
+    lines = open('file.txt','r')
+    read = lines.readlines()
+    lines.close()
+    print('OUTPUT', lines)
+    
+    
+    
+    
+    
+    
+
