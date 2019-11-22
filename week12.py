9f37b86f1fc919089c42ddae91ea66b52182ff68
James Garrison <jamesgarrison@csm-wl-dhcp-226-245.mines.edu>
2019-11-21 23:23:33 -0700

    def Intersection

--- week12.py
+++ week12.py
@@ -54,6 +54,11 @@ def ScoreFinder(list1, list2, string):
 def Union(list1, list2):
     list3 = list(set(list1) | set(list2))
     return list3
+
+# Function 7
+def Intersection(list1, list2):
+    list3 = list(set(list1) & set(list2))
+    return list3
     
     
     
